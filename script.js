1ace92ceb39600a1741223611eeb875af6611f20
bandardeterjen <bandardeterjen@gmail.com>
2025-02-08 08:36:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,4 @@ document.addEventListener('DOMContentLoaded', function () {
         navLinks.classList.toggle('active');
     });
 });
-document.querySelector(".menu-btn").addEventListener("click", abc);
-function abc() {
-  document.querySelector(".main-menu").classList.toggle("show");
-}
+
